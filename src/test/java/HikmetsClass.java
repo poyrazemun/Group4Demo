0203c24aa5d1ee79dfe67c6383eb8b458b295095
mhikmetozcan <mhikmetozcan@gmail.com>
2022-07-21 21:19:51 +0200

    Hikmet's third commit

--- src/test/java/HikmetsClass.java
+++ src/test/java/HikmetsClass.java
@@ -2,5 +2,6 @@ public class HikmetsClass {
     public static void main(String[] args) {
         System.out.println("Hello world from Hikmet");
         System.out.println("Printing for the second commit");
+        System.out.println("for the third one");
     }
 }
